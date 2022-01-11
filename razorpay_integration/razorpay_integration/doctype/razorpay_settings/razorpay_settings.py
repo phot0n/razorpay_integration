55d4a5a3c181c24790d4208bc46de6afc78303b7
phot0n <ritwikpuri5678@gmail.com>
2022-01-11 19:50:40 +0530

    minor: use expression for naming razorpay log
    * chore: remove reference if field from razorpay log

--- razorpay_integration/razorpay_integration/doctype/razorpay_settings/razorpay_settings.py
+++ razorpay_integration/razorpay_integration/doctype/razorpay_settings/razorpay_settings.py
@@ -35,29 +35,31 @@ class RazorpaySettings(Document):
 		kwargs["payer_email"] = frappe.session.user if (
 			frappe.session.user not in ("Guest", "Administrator")
 		) else ""
-		kwargs["payer_name"] = frappe.utils.get_fullname(frappe.session.user)
+		kwargs["payer_name"] = kwargs.get("payer_name", frappe.utils.get_fullname(frappe.session.user))
+
+		log = frappe.get_doc(
+			doctype="Razorpay Payment Log",
+			status="Created",
+			razorpay_setting=self.name,
+			description=kwargs.get("description"),
+			amount=flt(kwargs["amount"]),
+			payload=json.dumps(
+				dict(redirect_to=kwargs.get("redirect_to", "/"))
+			)
+		).insert(ignore_permissions=True)
+
+		# use log name as reference id in payment link
+		kwargs["reference_id"] = log.name
 
 		razorpay_response = RazorpayPayment(
 			self.api_key,
 			get_decrypted_password("Razorpay Settings", self.name, fieldname="api_secret")
 		).get_or_create_payment_link(**kwargs)
 
-		# log details in razorpay log
-		frappe.get_doc(
-			doctype="Razorpay Payment Log",
-			reference_id=razorpay_response.get("reference_id"),
-			status="Created",
-			razorpay_setting=self.name,
-			description=razorpay_response.get("description"),
-			currency=razorpay_response.get("currency"),
-			amount=flt(razorpay_response.get("amount") / 100), # razorpay returns the amount as it's sent to it
-			payment_link_id=razorpay_response.get("id"),
-			payment_url=razorpay_response.get("short_url"),
-			valid_till=razorpay_response.get("expire_by"),
-			customer=json.dumps(razorpay_response.get("customer")),
-			payload=json.dumps(
-				dict(redirect_to=kwargs.get("redirect_to", "/"))
-			)
-		).insert(ignore_permissions=True)
+		log.payment_link_id = razorpay_response.get("id")
+		log.payment_url = razorpay_response.get("short_url")
+		log.valid_till = razorpay_response.get("expire_by")
+		log.customer = json.dumps(razorpay_response.get("customer"))
+		log.save(ignore_permissions=True)
 
 		return razorpay_response.get("short_url")
